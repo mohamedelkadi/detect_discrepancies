4d0090cbab33891362418d975280bbbeb772882b
molhm <molhm66@gmail.com>
2019-07-15 18:02:39 +0200

    Improve RemoteCampaign specs

--- spec/detect_discrepancies/remote_campaign_spec.rb
+++ spec/detect_discrepancies/remote_campaign_spec.rb
@@ -2,16 +2,20 @@ RSpec.describe DetectDiscrepancies::RemoteCampaign do
   before do
     body = '{ "ads": [ { "reference": "1", "status": "enabled", "description": "Description for campaign 11" }, { "reference": "2", "status": "disabled", "description": "Description for campaign 12" }, { "reference": "3", "status": "enabled", "description": "Description for campaign 13" } ] }'
 
-    stub_request(:get, 'http://dummy.com/ref-1').to_return(body: body)
+    stub_request(:get, 'http://dummy.com').to_return(body: body)
   end
 
-  describe '#fetch_ads' do
+  describe '#find_ad' do
     subject {
-      described_class.new('http://dummy.com/ref-1')
+      described_class.new('http://dummy.com')
     }
 
-    it 'returns instance of RemoteCampaign' do
+    it 'finds the ad with the correct reference' do
       expect(subject.find_ad('1').reference).to eq('1')
+    end
+
+    it 'finds the ad with the expected status' do
+      expect(subject.find_ad('1').status).to eq('enabled')
     end
   end
 end
